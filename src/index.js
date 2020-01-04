f77f3f3a9ce5ea81f007b3d6633bd185a541d694
tyler.keesling <tyler.keesling@import.io>
2020-01-04 11:44:26 -0600

    Set up global styles and themes

--- src/index.js
+++ src/index.js
@@ -1,12 +1,64 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import styled, { injectGlobal, ThemeProvider } from 'styled-components';
+import styled, { createGlobalStyle, ThemeProvider } from 'styled-components';
+import metropolis from './static/Metropolis-Regular.woff';
+
+const theme = {
+  black: '#393939',
+  grey: '#3A3A3A',
+  lightGrey: '#E1E1E1',
+  offWhite: '#EDEDED',
+  maxWidth: '1000px',
+};
+
+const StyledApp = styled.div`
+  background: white;
+  color: ${props => props.theme.black};
+`;
+
+const Inner = styled.div`
+  max-width: ${props => props.theme.maxWidth};
+  margin: 0 auto;
+  padding: 2rem;
+`;
+
+const GlobalStyle = createGlobalStyle`
+  @font-face {
+    font-family: 'metropolis';
+    src: url(${metropolis}) format("woff");
+    font-weight: normal;
+    font-style: normal;
+  }
+  html {
+    box-sizing: border-box;
+    font-size: 10px;
+  }
+  *, *:before, *:after {
+    box-sizing: inherit;
+  }
+  body {
+    padding: 0;
+    margin: 0;
+    font-size: 1.5rem;
+    line-height: 2;
+    font-family: metropolis, 'serif';
+  }
+  a {
+    text-decoration: none;
+    color: ${theme.black};
+  }
+`;
 
 function App() {
   return (
-    <div className='App'>
-      <h1>Hello World</h1>
-    </div>
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <StyledApp>
+        <Inner>
+          <p>Hello World</p>
+        </Inner>
+      </StyledApp>
+    </ThemeProvider>
   );
 }
 
