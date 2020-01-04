345b1bd8051434255884cc07cbe0eb0f9fbdde4c
tyler.keesling <tyler.keesling@import.io>
2020-01-04 10:12:27 -0600

    Configuring project linters, etc

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import styled, { injectGlobal, ThemeProvider } from 'styled-components';
+
+function App() {
+  return (
+    <div className='App'>
+      <h1>Hello World</h1>
+    </div>
+  );
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
