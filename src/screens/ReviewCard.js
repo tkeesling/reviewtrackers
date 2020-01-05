fcc8f5c9fb21b89d7322ed0f368f0549413edce0
tyler.keesling <tyler.keesling@import.io>
2020-01-05 00:04:42 -0600

    Adjusting propTypes

--- src/screens/ReviewCard.js
+++ src/screens/ReviewCard.js
@@ -17,11 +17,6 @@ const StyledReviewCard = styled.div`
     display: grid;
     width: 100%;
     grid-template-columns: repeat(2, minmax(200px, 1fr));
-    & > * {
-      background: white;
-      border: 0;
-      /* padding: 1rem; */
-    }
   }
 `;
 
@@ -40,14 +35,14 @@ const Date = styled.span`
 `;
 
 function ReviewCard({ review }) {
-  const { id, author, place, published_at, rating } = review;
+  const { id, author, place, published_at: publishedAt, rating } = review;
   return (
     <StyledReviewCard>
       <Title>{place}</Title>
       <StarRow rating={rating} />
       <div className="author-date">
         <Author>{author}</Author>
-        <Date>{formatDateTime(published_at)}</Date>
+        <Date>{formatDateTime(publishedAt)}</Date>
       </div>
     </StyledReviewCard>
   );
@@ -58,17 +53,11 @@ ReviewCard.propTypes = {
     PropTypes.shape({
       id: PropTypes.string.isRequired,
       author: PropTypes.string.isRequired,
+      place: PropTypes.string.isRequired,
+      published_at: PropTypes.string.isRequired,
+      rating: PropTypes.string.isRequired,
     })
   ),
 };
 
 export default ReviewCard;
-
-// {
-//   "id": "5d707203b65083001e956f0a",
-//   "author": "Weeks Duran",
-//   "place": "Big Johns Burgers",
-//   "published_at": "Thu Jun 27 1974 11:46:39 GMT-0500 (Central Daylight Time)",
-//   "rating": 5,
-//   "content": "Ipsum mollit anim pariatur eu qui velit Lorem ea enim excepteur ut fugiat fugiat esse. Incididunt consectetur deserunt pariatur magna sit dolore voluptate. Minim cupidatat fugiat magna quis consectetur esse id esse adipisicing anim velit. Cillum mollit et nisi ex occaecat labore enim nulla cupidatat. Occaecat Lorem officia est sit enim amet commodo sunt occaecat reprehenderit Lorem culpa. Aute anim ullamco voluptate incididunt incididunt excepteur in irure.\r\n"
-// },
