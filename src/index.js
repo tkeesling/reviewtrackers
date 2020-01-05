619aa570072ba1f3d9acbd4fb7ac7945aa4f5259
tyler.keesling <tyler.keesling@import.io>
2020-01-05 08:53:43 -0600

    Add router for pages; refactor to page component

--- src/index.js
+++ src/index.js
@@ -1,73 +1,16 @@
+import { Router } from '@reach/router';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import styled, { createGlobalStyle, ThemeProvider } from 'styled-components';
+import Page from './components/Page';
 import ReviewsPage from './pages/ReviewsPage';
-import metropolisBold from './static/Metropolis-Bold.woff';
-import metropolis from './static/Metropolis-Regular.woff';
-
-const theme = {
-  black: '#393939',
-  grey: '#9D9D9D',
-  lightGrey: '#E1E1E1',
-  offWhite: '#EDEDED',
-  maxWidth: '1000px',
-  boxShadow: '0 12px 24px 0 rgba(0, 0, 0, 0.09)',
-};
-
-const StyledApp = styled.div`
-  background: white;
-  color: ${props => props.theme.black};
-`;
-
-const Inner = styled.div`
-  max-width: ${props => props.theme.maxWidth};
-  margin: 0 auto;
-  padding: 2rem;
-`;
-
-const GlobalStyle = createGlobalStyle`
-  @font-face {
-    font-family: 'metropolis';
-    src: url(${metropolis}) format("woff");
-    font-weight: normal;
-    font-style: normal;
-  }
-  @font-face {
-    font-family: 'metropolis-bold';
-    src: url(${metropolisBold});
-    font-weight: normal;
-    font-style: normal;
-  }
-  html {
-    box-sizing: border-box;
-    font-size: 10px;
-  }
-  *, *:before, *:after {
-    box-sizing: inherit;
-  }
-  body {
-    padding: 0;
-    margin: 0;
-    font-size: 1.5rem;
-    line-height: 2;
-    font-family: metropolis, serif;
-  }
-  a {
-    text-decoration: none;
-    color: ${theme.black};
-  }
-`;
 
 function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <GlobalStyle />
-      <StyledApp>
-        <Inner>
-          <ReviewsPage />
-        </Inner>
-      </StyledApp>
-    </ThemeProvider>
+    <Page>
+      <Router>
+        <ReviewsPage path="/" />
+      </Router>
+    </Page>
   );
 }
 
